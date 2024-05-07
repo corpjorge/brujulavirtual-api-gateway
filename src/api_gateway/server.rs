3790bcf39239f14e8aafb3eec9f1e0ec37c1679b
corpjorge <corpjorge@hotmail.com>
2024-05-06 23:55:47 -0500

    feat: add .env

--- src/api_gateway/server.rs
+++ src/api_gateway/server.rs
@@ -1,26 +1,33 @@
-use actix_web::{App, HttpServer, web};
-use reqwest::Client;
+use std::env;
 use std::sync::Arc;
+use dotenv::dotenv;
+use reqwest::Client;
+use actix_web::{App, HttpServer, web};
 use crate::services::auth_service::AuthService;
 use crate::api_gateway::router::configure_routes;
 
 pub async fn run_server() -> std::io::Result<()> {
 
+    dotenv().ok();
+
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let server_address = format!("0.0.0.0:{}", port);
+
+
+    let auth_service_url = env::var("AUTH_SERVICE_URL").unwrap_or_else(|_| "http://localhost:8081".to_string());
+
     let client = Arc::new(Client::new());
 
-    let auth_service = web::Data::new(AuthService::new(
-        "http://localhost:8081".to_string(),
-        client.clone(),
-    ));
+    let auth_service = web::Data::new(AuthService::new(auth_service_url, client.clone()));
 
-    let bind_address = "0.0.0.0:8080";
+    println!("API Gateway running at http://{}", server_address);
 
     HttpServer::new(move || {
         App::new()
             .app_data(auth_service.clone())
             .configure(configure_routes)
     })
-        .bind(bind_address)?
+        .bind(server_address)?
         .run()
         .await
 }
